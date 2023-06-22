8bb43f471a45624c86cefc86edb13424d5ff764a
SoraKumo <info@croud.jp>
2023-06-22 21:14:59 +0900

    fix command quart

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
-#!/usr/bin/env node
-
 import path from "path";
-import spawn from "cross-spawn";
 import dotenv from "dotenv";
 import { expand } from "dotenv-expand";
-import minimist from "minimist";
-
-const printHelp = () => {
-  console.log(
-    [
-      "Usage: next-exec [-c <environment>] -- [command]",
-      "  -c <environment>    development , production , test , etc…",
-      "                      default: process.env.NODE_ENV ?? development",
-      "                      https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables",
-      "  command             Commands to execute",
-    ].join("\n")
-  );
-};
 
 export const getEnvFiles = (node_env: string) => {
   return [
@@ -33,29 +17,3 @@ export const loadNextEnv = (node_env?: string) => {
     expand(dotenv.config({ path: path.resolve(env) }));
   });
 };
-
-const main = () => {
-  const argv = process.argv.slice(2);
-  const index = argv.findIndex((v) => v === "--");
-  const option = index >= 0 ? argv.slice(0, index) : [];
-  const command = index >= 0 ? argv.slice(index + 1) : argv;
-  const params = minimist(option);
-
-  if (params.help || argv.length === 0) {
-    printHelp();
-    process.exit();
-  }
-
-  loadNextEnv(params.c);
-
-  if (command) {
-    spawn(command[0], command.slice(1), { stdio: "inherit", shell: true }).on(
-      "exit",
-      function (exitCode: number) {
-        process.exit(exitCode);
-      }
-    );
-  }
-};
-
-main();
---
+++ src/next-exec.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import spawn from "cross-spawn";
+import minimist from "minimist";
+// eslint-disable-next-line import/no-unresolved
+import { loadNextEnv } from "./index";
+
+const printHelp = () => {
+  console.log(
+    [
+      "Usage: next-exec [-c <environment>] -- [command]",
+      "  -c <environment>    development , production , test , etc…",
+      "                      default: process.env.NODE_ENV ?? development",
+      "                      https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables",
+      "  command             Commands to execute",
+    ].join("\n")
+  );
+};
+
+const main = () => {
+  const argv = process.argv.slice(2);
+  const index = argv.findIndex((v) => v === "--");
+  const option = index >= 0 ? argv.slice(0, index) : [];
+  const command = index >= 0 ? argv.slice(index + 1) : argv;
+  const params = minimist(option);
+
+  if (params.help || argv.length === 0) {
+    printHelp();
+    process.exit();
+  }
+
+  loadNextEnv(params.c);
+  if (command) {
+    spawn(
+      command[0],
+      command.slice(1).map((v) => v.replace(/"/g, '\\"')),
+      { stdio: "inherit", shell: true }
+    ).on("exit", function (exitCode: number) {
+      process.exit(exitCode);
+    });
+  }
+};
+
+main();
